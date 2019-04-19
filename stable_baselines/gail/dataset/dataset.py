89b7a8a66c71e17df073c8e2c7f4c507e1dda9da
Adam Gleave <adam@gleave.me>
2019-04-18 20:53:56 -0700

    Swap parameter order to maintain some backwards compatibility

--- stable_baselines/gail/dataset/dataset.py
+++ stable_baselines/gail/dataset/dataset.py
@@ -18,8 +18,8 @@ class ExpertDataset(object):
     'rewards', 'obs', 'episode_starts'
     In case of images, 'obs' contains the relative path to the images.
 
-    :param traj_data: (dict) Trajectory data, in format described above. Mutually exclusive with expert_path.
     :param expert_path: (str) The path to trajectory data (.npz file). Mutually exclusive with traj_data.
+    :param traj_data: (dict) Trajectory data, in format described above. Mutually exclusive with expert_path.
     :param train_fraction: (float) the train validation split (0 to 1)
         for pre-training using behavior cloning (BC)
     :param batch_size: (int) the minibatch size for behavior cloning
@@ -30,7 +30,7 @@ class ExpertDataset(object):
         the data (slower but use less memory for the CI)
     """
 
-    def __init__(self, traj_data=None, expert_path=None, train_fraction=0.7, batch_size=64,
+    def __init__(self, expert_path=None, traj_data=None, train_fraction=0.7, batch_size=64,
                  traj_limitation=-1, randomize=True, verbose=1, sequential_preprocessing=False):
         if traj_data is not None and expert_path is not None:
             raise ValueError("Cannot specify both 'traj_data' and 'expert_path'")
