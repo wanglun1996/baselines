54033f52f40f609a6e44b3a5f0346846df267e37
wanglun1996 <wanglun@berkeley.edu>
2019-12-23 11:09:22 +0800

    allow flesible batch size

--- stable_baselines/common/policies.py
+++ stable_baselines/common/policies.py
@@ -427,8 +427,10 @@ class LstmPolicy(RecurrentActorCriticPolicy):
                     for i, layer_size in enumerate(layers):
                         extracted_features = act_fun(linear(extracted_features, 'pi_fc' + str(i), n_hidden=layer_size,
                                                             init_scale=np.sqrt(2)))
-                input_sequence = batch_to_seq(extracted_features, self.n_env, n_steps)
-                masks = batch_to_seq(self.dones_ph, self.n_env, n_steps)
+                # input_sequence = batch_to_seq(extracted_features, self.n_env, n_steps)
+                # masks = batch_to_seq(self.dones_ph, self.n_env, n_steps)
+                input_sequence = [extracted_features]
+                masks = [tf.reshape(self.dones_ph, [-1, 1])]
                 rnn_output, self.snew = lstm(input_sequence, masks, self.states_ph, 'lstm1', n_hidden=n_lstm,
                                              layer_norm=layer_norm)
                 rnn_output = seq_to_batch(rnn_output)
