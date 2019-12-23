559221487241208eacfabab89bcc1dd51e7fb421
wanglun1996 <wanglun@berkeley.edu>
2019-12-23 08:39:16 +0800

    add pretrain_lstm

--- stable_baselines/ppo2/ppo2.py
+++ stable_baselines/ppo2/ppo2.py
@@ -88,15 +88,28 @@ class PPO2(ActorCriticRLModel):
         self.n_batch = None
         self.summary = None
         self.episode_reward = None
+        # !!! new added
+        self.loaded = False
 
         if _init_setup_model:
             self.setup_model()
+
+    @classmethod
+    def load(cls, load_path):
+        super(PPO2, cls).load(load_path)
+        cls.loaded = True
 
     def _get_pretrain_placeholders(self):
         policy = self.act_model
         if isinstance(self.action_space, gym.spaces.Discrete):
             return policy.obs_ph, self.action_ph, policy.policy
         return policy.obs_ph, self.action_ph, policy.deterministic_action
+
+    def _get_lstm_pretrain_placeholders(self):
+        policy = self.act_model
+        # if isinstance(self.action_space, gym.spaces.Discrete):
+        #     return policy.obs_ph, self.action_ph, policy.policy
+        return policy.obs_ph, self.action_ph, policy.dones_ph, policy.states_ph, policy.deterministic_action
 
     def setup_model(self):
         with SetVerbosity(self.verbose):
@@ -208,6 +221,106 @@ class PPO2(ActorCriticRLModel):
                 tf.global_variables_initializer().run(session=self.sess)  # pylint: disable=E1101
 
                 self.summary = tf.summary.merge_all()
+
+    def pretrain_lstm(self, dataset, n_epochs=10, learning_rate=1e-4, adam_epsilon=1e-8, val_interval=None):
+
+       if str(self.policy) == "<class 'stable_baselines.common.policies.MlpPolicy'>":
+           super(MyPPO2, self).pretrain(dataset, n_epochs=n_epochs, learning_rate=learning_rate, adam_epsilon=adam_epsilon, val_interval=val_interval) 
+
+       elif str(self.policy) == "<class 'stable_baselines.common.policies.MlpLstmPolicy'>":
+           continuous_actions = isinstance(self.action_space, gym.spaces.Box)
+           discrete_actions = isinstance(self.action_space, gym.spaces.Discrete)
+
+           assert discrete_actions or continuous_actions, 'Only Discrete and Box action spaces are supported'
+
+           # Validate the model every 10% of the total number of iteration
+           if val_interval is None:
+               # Prevent modulo by zero
+               if n_epochs < 10:
+                   val_interval = 1
+               else:
+                   val_interval = int(n_epochs / 10)
+
+           with self.graph.as_default():
+               with tf.variable_scope('pretrain'):
+                   if continuous_actions:
+                       obs_ph, actions_ph, dones_ph, states_ph, deterministic_actions_ph = self._get_lstm_pretrain_placeholders()
+                       loss = tf.reduce_mean(tf.square(actions_ph - deterministic_actions_ph))
+                   # else:
+                       # obs_ph, actions_ph, actions_logits_ph = self._get_pretrain_placeholders()
+                       # actions_ph has a shape if (n_batch,), we reshape it to (n_batch, 1)
+                       # so no additional changes is needed in the dataloader
+                       # actions_ph = tf.expand_dims(actions_ph, axis=1)
+                       # one_hot_actions = tf.one_hot(actions_ph, self.action_space.n)
+                       # loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+                       #     logits=actions_logits_ph,
+                       #     labels=tf.stop_gradient(one_hot_actions)
+                       # )
+                       # loss = tf.reduce_mean(loss)
+                   optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, epsilon=adam_epsilon)
+                   optim_op = optimizer.minimize(loss, var_list=self.params)
+
+               if not self.loaded:
+                   self.sess.run(tf.global_variables_initializer())
+
+           if self.verbose > 0:
+               print("Pretraining with Behavior Cloning...")
+
+           for epoch_idx in range(int(n_epochs)):
+               # print('epoch_id: ' + str(epoch_idx))
+               train_loss = 0.0
+               states = self.act_model.initial_state
+               # Full pass on the training set
+               for _ in range(dataset.max_train_traj_length):
+                   # print(i)
+                   expert_obs, expert_actions, expert_dones = dataset.get_next_batch(split='train')
+                   # print(expert_obs)
+                   feed_dict = {
+                       obs_ph: expert_obs,
+                       actions_ph: expert_actions,
+                       dones_ph: expert_dones,
+                       states_ph: states,
+                   }
+                   states, train_loss_, _ = self.sess.run([self.act_model.snew, loss, optim_op], feed_dict)
+                   for idx, done in enumerate(expert_dones):
+                       if done:
+                           states[idx] = np.zeros(states[idx].shape) 
+                   train_loss += train_loss_
+
+               train_loss /= dataset.max_train_traj_length
+
+               if self.verbose > 0 and (epoch_idx + 1) % val_interval == 0:
+                   val_loss = 0.0
+                   states = self.act_model.initial_state
+                   # Full pass on the validation set
+                   for _ in range(dataset.max_train_traj_length):
+                       expert_obs, expert_actions, expert_dones = dataset.get_next_batch(split='val')
+                       feed_dict = {
+                           obs_ph: expert_obs,
+                           actions_ph: expert_actions,
+                           dones_ph: expert_dones,
+                           states_ph: states,
+                       }
+                       states, val_loss_ = self.sess.run([self.act_model.snew, loss], feed_dict)
+                       for idx, done in enumerate(expert_dones):
+                           if done:
+                               states[idx] = np.zeros(states[idx].shape)
+                       val_loss += val_loss_
+
+                   val_loss /= dataset.max_val_traj_length
+
+                   if self.verbose > 0:
+                       print("==== Training progress {:.2f}% ====".format(100 * (epoch_idx + 1) / n_epochs))
+                       print('Epoch {}'.format(epoch_idx + 1))
+                       print("Training loss: {:.6f}, Validation loss: {:.6f}".format(train_loss, val_loss))
+                       print()
+               # Free memory
+               del expert_obs, expert_actions
+           if self.verbose > 0:
+               print("Pretraining done.")
+           return self
+       else:
+           raise NotImplementedError
 
     def _train_step(self, learning_rate, cliprange, obs, returns, masks, actions, values, neglogpacs, update,
                     writer, states=None):
